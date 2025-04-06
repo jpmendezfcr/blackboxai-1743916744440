109ba30fc1f1ba8d3c029112afe65a6ef4583ab5
jpmendezfcr <juanpablomendezfonsecacr@gmail.com>
2025-04-05 23:20:53 -0600

    Add client/src/components/index.js 1743916796911

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CustomInput } from './CustomInput';
+export { default as CustomButton } from './CustomButton';
+export { default as LoadingOverlay } from './LoadingOverlay';
+export { default as AlertModal } from './AlertModal';
